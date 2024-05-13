1e9096de98e04aff402b07c5acc13a9696985ca4
emyrysf <bolhocaliogluemiryusuf@gmail.com>
2024-05-13 19:40:55 +0300

    Update Plant.cs
    Plant ana kodu

--- Overgrown/Assets/Scripts/Plant.cs
+++ Overgrown/Assets/Scripts/Plant.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plant : MonoBehaviour
+[System.Serializable]
+public class Plant 
 {
-    // Start is called before the first frame update
-    void Start()
+    private string plantName;
+    private float growTime;
+    private float overgrownTime;
+    private GameObject product;
+    private float initialTime;
+    bool isWatered;
+
+    public Plant(string name, float gTime, float ogTime, GameObject pr)
     {
-        
+        plantName = name;
+        growTime = gTime;
+        overgrownTime = ogTime;
+        product = pr;
+        isWatered = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public bool ReadyToHarvest
     {
-        
+        get
+        {
+            if (isWatered)
+                return Time.time-initialTime >= growTime;
+            return false;
+        }
     }
+
+    public bool IsOverGrown
+    {
+        get
+        {   if (isWatered)
+                return Time.time - initialTime >= overgrownTime;
+            else return false;
+        }
+    }
+
+    public GameObject Harvest() 
+    { 
+        if (ReadyToHarvest)
+            return product;
+        else 
+            return null;
+    }
+
+    public void Water()
+    {
+        initialTime = Time.time;
+        isWatered = true;
+    }
+
+
+
+
 }
