49d080f917c0d0a75ea00cea68c6c2e6e4e48421
ot113 <158476901+ot113@users.noreply.github.com>
2024-05-17 23:54:34 +0300

    Character Rotation Bug Solved
    olum kübü yanlış yere koymuşum aw

--- Overgrown/Assets/Scripts/ThirdPersonCharacterMovement.cs
+++ Overgrown/Assets/Scripts/ThirdPersonCharacterMovement.cs
@@ -1,20 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class ThirdPersonCharacterMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public Transform cam;
 
     public float speed = 6f;
-
     public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
 
-    float turnSmoothVelocity;
-
-    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -23,12 +18,26 @@ public class ThirdPersonCharacterMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            // Calculate the target angle based on input direction
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            // Smoothly transition to the target angle
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            // Calculate the move direction
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            // Move the character
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
